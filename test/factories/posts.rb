3e3ddce004552d7d951b7a8e7548c85670e99439
Jovin Angelico <jovin.angelico@live.com>
2019-01-09 00:20:55 +0700

    Add author id on factory bot post

--- test/factories/posts.rb
+++ test/factories/posts.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
     body { FFaker::Lorem.paragraphs(paragraph_count = 5) }
     description { FFaker::Lorem.paragraph(sentence_count = 10) }
     banner_image_url { "https://picsum.photos/600/400/?image=#{ FFaker::PhoneNumber.area_code}"}
+    author_id { 1 }
   end
 end
