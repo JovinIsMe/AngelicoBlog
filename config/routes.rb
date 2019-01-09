9c5058e171d2e37988863036e65c7fbfa3efbf2f
Jovin Angelico <jovin.angelico@live.com>
2019-01-09 22:35:05 +0700

    Revert "Add login button on navigation bar"
    This reverts commit 4039a8835b5530a40e1d4ce4fbd233d64264378d.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'authors/sign_in', as: :login
-
   devise_for :authors
   root to: 'blog/posts#index'
 
@@ -20,6 +18,5 @@ Rails.application.routes.draw do
     get 'posts/new' => 'posts#new', as: :new_post
     get 'posts/:id'=> 'posts#show', as: :post
   end
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
